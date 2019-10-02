ed35cd94a98fbc2656b8bda8830348327c3bebc4
Olivier GATISSOU <oli.gatissou@gmail.com>
2019-10-02 19:20:49 +0200

    Implementation de la methode getCountBookAddedByMont

--- src/book.repository.js
+++ src/book.repository.js
@@ -72,7 +72,100 @@ class BookRepository {
      *      ....
      *  ]
      */
+
     getCountBookAddedByMont(bookName) {
+
+        let countBookTab = [];
+
+        if(typeof bookName === "undefined"){
+            throw 'Le paramètre n\'est pas valide';
+        }
+        if(bookName==="" || bookName===" "){
+            throw 'Le titre du livre ne peut être vide';
+        }
+
+        if(this.db.get('books').size().value()===0){
+            throw "Il n'y a pas de livre en db";
+        }
+
+        let map = new Map();
+        let currentA, dateSplit;
+        let annee, month;
+        let books = this.db.get('books').filter({name:bookName}).sortBy('added_at').value();
+        for (let book of books){
+            dateSplit = book.added_at.split('-');
+            annee = dateSplit[0];
+            month = dateSplit[1];
+            currentA = map.get(annee);
+            if(currentA === undefined){
+                currentA = new Map();
+                map.set(annee, currentA);
+            }
+            if(currentA.get(month) === undefined){
+                currentA.set(month,1);
+            }
+            else {
+                currentA.set(month,currentA.get(month)+1);
+            }
+        }
+
+        countBookTab = this.updateOut(map);
+
+        return countBookTab;
+
+    }
+
+
+    getCountBookAddedByMonth() {
+
+        let countBookTab = [];
+
+        if(this.db.get('books').size().value()===0){
+            throw "Il n'y a pas de livre en db";
+        }
+
+        let map = new Map();
+        let currentA, dateSplit;
+        let annee, month;
+        let books = this.db.get('books').sortBy('added_at').value();
+        for (let book of books){
+            dateSplit = book.added_at.split('-');
+            annee = dateSplit[0];
+            month = dateSplit[1];
+            currentA = map.get(annee);
+            if(currentA === undefined){
+                currentA = new Map();
+                map.set(annee, currentA);
+            }
+            if(currentA.get(month) === undefined){
+                currentA.set(month,1);
+            }
+            else {
+                currentA.set(month,currentA.get(month)+1);
+            }
+        }
+
+        countBookTab = this.updateOut(map);
+
+        return countBookTab;
+    }
+
+    updateOut(map) {
+        let countBookTab = [];
+        let cumulBooks;
+        map.forEach(function (valueMap, keyMap) {
+            cumulBooks = 0;
+            valueMap.forEach(function (valueMois, keyMois) {
+                cumulBooks += valueMois;
+                countBookTab.push({
+                    year: keyMap,
+                    month: keyMois,
+                    count: valueMois,
+                    count_cumulative: cumulBooks
+                });
+            });
+        });
+        return countBookTab;
     }
 
 }
