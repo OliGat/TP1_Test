4259a4affe1127eea2287fb0b6048d6131cab6ea
Olivier GATISSOU <oli.gatissou@gmail.com>
2019-10-02 19:19:48 +0200

    Ajout de parametres de configuration

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,10 +1,19 @@
 module.exports = function(config) {
   config.set({
-    mutator: "javascript",
+    mutator: {
+      name: 'javascript',
+      excludedMutations: ['BooleanSubstitution', 'StringLiteral']
+    },
     packageManager: "npm",
     reporters: ["html", "clear-text", "progress", "dashboard"],
     testRunner: "jest",
     transpilers: [],
-    coverageAnalysis: "off"
+    coverageAnalysis: "off",
+    thresholds: { high: 80, low: 60, break: null },
+    files: [
+        'src/**/*.js',
+        '!src/db.js',
+        '!src/test-script.js',
+        'test/**/*.js']
   });
 };
